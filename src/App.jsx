f232e769d9409698aeda92cfb60cd1ab992c7224
Thomas Dietrich <tomriiich@gmail.com>
2021-05-11 22:13:49 +0200

    Vine branch first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Vine from './component/Vine';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Vine />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Vine.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Vine.css';
+
+function Vine() {
+    return (
+        <div className="Vine">
+          <div className="gameView">
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511080049371746.png" alt="vine" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511100509192861.png" alt="corn" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511100815916143.png" alt="sunflower" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101215927332.png" alt="honey" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511072340541329.png" alt="mill" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101325344863.png" alt="sprout" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/21051110135135111.png" alt="tree" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101411434825.png" alt="well" width="150px" />
+            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101430539210.png" alt="wheat" width="150px" />
+          </div>
+        </div>
+    )
+  }
+
+  export default Vine;
+  
