95343614a957aa6e379d6767a650ecc41f821a6d
calvinhyacinth@gmail.com <calvinhyacinth@gmail.com>
2021-05-11 23:47:04 +0200

    added buttons to the Plant page and pass props from app page to the plants page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Plants from './component/Plants';
 import './App.css';
 
 function App() {
+  const [tree, setTree] = useState(false);
+  const [sunflower, setSunflower] = useState(false);
+  const [hive, setHive] = useState(false);
+  const [sprout, setSprout] = useState(false);
+  const [corn, setCorn] = useState(false);
+
   return (
-      <Plants />
+      <Plants 
+      setTree={setTree}
+      setSunflower={setSunflower}
+      setHive={setHive}
+      setSprout={setSprout}
+      setCorn={setCorn}
+      />
   );
 }
 
--- src/component/Plants.jsx
+++ src/component/Plants.jsx
@@ -1,57 +1,47 @@
 import React from 'react';
 import './Plants.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { GiPlantRoots } from 'react-icons/gi';
-import { GiBeehive } from 'react-icons/gi';
-import { GiRiver } from 'react-icons/gi';
-import { GiTomato } from 'react-icons/gi';
-import { GiFruitTree } from 'react-icons/gi';
+import  Sunflower from './photos/field_of_sunflower.png';
+import  Hive from './photos/honeycomb.png';
+import  Tree from './photos/tree.png';
+import  Sprout from './photos/sprout.png';
+import Corn from './photos/corn_field.png';
 
-
-
-
-const Plants = () => (
-    <Router>
+const Plants = ({ setTree, setSunflower, setHive, setSprout, setCorn }) => (
+       <>
         <div className="plantsHeader">
           <h1>Plants/Levels</h1> 
         </div>
-      <Link className="link" activeClassName="active">
-        <div>
-       <GiPlantRoots
-         size={35} />
-       <p>Roquette</p>
-        </div>
-      </Link>
-      <Link className="link" activeClassName="active">
-      <div>
-       <GiBeehive
-         size={35} />
+      <button className="link" onClick={ ()=> setSunflower(true)} activeClassName="active">
+        <div className="plants">
+        <img  className="pics" src={Sunflower} alt="sunflower" />     
+       <p>Sun flower</p>
+       </div>
+      </button>
+      <button className="link" onClick={ ()=> setHive(true)} activeClassName="active">
+      <div className="plants">
+      <img  className="pics" src={Hive} alt="Hive" />     
        <p>Hive</p>
-        </div> 
-      </Link>
-      <Link className="link" activeClassName="active">
-      <div>
-       <GiRiver
-         size={35} />
-       <p>Pond</p>
-        </div> 
-      </Link>
-      <Link className="link" activeClassName="active">
-      <div>
-       <GiTomato
-         size={35} />
-       <p>Tomatoes</p>
-        </div> 
-      </Link>
-      <Link className="link" activeClassName="active">
-      <div>
-       <GiFruitTree
-         size={35} />
-       <p>Trees</p>
-        </div> 
-      </Link>
-    
-    </Router>
+       </div>
+      </button>
+      <button className="link" onClick={ ()=> setTree(true)} activeClassName="active">
+      <div className="plants">
+      <img  className="pics" src={Tree} alt="Tree" />     
+       <p>Tree</p>
+       </div>
+      </button>
+      <button className="link" onClick={ ()=> setSprout(true)} activeClassName="active">
+      <div className="plants">
+      <img  className="pics" src={Sprout} alt="Sprout" />    
+       <p>Sprout</p>
+       </div>
+      </button>
+      <button className="link" onClick={ ()=> setCorn(true)} activeClassName="active">
+      <div className="plants">
+      <img  className="pics" src={Corn} alt="Corn" />     
+       <p>Corn</p>
+       </div>
+      </button>
+      </>
     );
 
 export default Plants;
