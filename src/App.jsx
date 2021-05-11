8a0e0dba9bb399726bdd3d9833b496fe585bf8c9
Thomas Dietrich <tomriiich@gmail.com>
2021-05-12 00:04:15 +0200

    Vine branch second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img className="lplg-logo" src="https://nsa40.casimages.com/img/2021/05/11/210511104346329002.png" alt="lplg-logo" width="150px" />
       <Vine />
     </div>
   );
--- src/component/Vine.jsx
+++ src/component/Vine.jsx
@@ -5,15 +5,15 @@ function Vine() {
     return (
         <div className="Vine">
           <div className="gameView">
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511080049371746.png" alt="vine" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511100509192861.png" alt="corn" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511100815916143.png" alt="sunflower" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101215927332.png" alt="honey" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511072340541329.png" alt="mill" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101325344863.png" alt="sprout" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/21051110135135111.png" alt="tree" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101411434825.png" alt="well" width="150px" />
-            <img src="https://nsa40.casimages.com/img/2021/05/11/210511101430539210.png" alt="wheat" width="150px" />
+            <img className="vine-img" src="https://nsa40.casimages.com/img/2021/05/11/210511080049371746.png" alt="vine" />
+            <img className="corn-img" src="https://nsa40.casimages.com/img/2021/05/11/210511100509192861.png" alt="corn" />
+            <img className="sunflower-img" src="https://nsa40.casimages.com/img/2021/05/11/210511100815916143.png" alt="sunflower" />
+            <img className="honey-img" src="https://nsa40.casimages.com/img/2021/05/11/210511101215927332.png" alt="honey" />
+            <img className="mill-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120247345062.png" alt="mill" />
+            <img className="sprout-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120321473004.png" alt="sprout" />
+            <img className="tree-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120221548686.png" alt="tree" />
+            <img className="well-img" src="https://nsa40.casimages.com/img/2021/05/11/210511101411434825.png" alt="well" />
+            <img className="wheat-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120139894866.png" alt="wheat" />
           </div>
         </div>
     )
