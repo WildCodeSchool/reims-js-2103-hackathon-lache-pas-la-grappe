e0a5c59d9a9da5cd3f107f6fb240414219448131
Banzaipj <jolypascal20@gmail.com>
2021-05-11 15:34:30 +0200

    Added Navbar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav className="navbar">
+  <div>
+      <ul className="navbarLink">
+        <li><button type = "button" className="navbarLinkItem"> Vine </button></li>
+        <li><button type = "button" className="navbarLinkItem"> Plants / Levels </button></li>
+        <li><button type = "button" className="navbarLinkItem"> Map </button></li>
+      </ul>
+     </div>
+
+    </nav>
+  )
+
+  export default Navbar
