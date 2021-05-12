a74f67430d1f0d2a4d8bc8adb5a0f4bfbb24af65
Thomas Dietrich <tomriiich@gmail.com>
2021-05-12 09:33:12 +0200

    CSS on vine page

--- src/component/Vine.jsx
+++ src/component/Vine.jsx
@@ -6,14 +6,23 @@ function Vine() {
         <div className="Vine">
           <div className="gameView">
             <img className="vine-img" src="https://nsa40.casimages.com/img/2021/05/11/210511080049371746.png" alt="vine" />
+            <p className="vine-health">10/100</p>
             <img className="corn-img" src="https://nsa40.casimages.com/img/2021/05/11/210511100509192861.png" alt="corn" />
+            <p className="corn-points">+10</p>
             <img className="sunflower-img" src="https://nsa40.casimages.com/img/2021/05/11/210511100815916143.png" alt="sunflower" />
+            <p className="sunflower-points">+15</p>
             <img className="honey-img" src="https://nsa40.casimages.com/img/2021/05/11/210511101215927332.png" alt="honey" />
+            <p className="honey-points">+20</p>
             <img className="mill-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120247345062.png" alt="mill" />
+            <p className="mill-points">+30</p>
             <img className="sprout-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120321473004.png" alt="sprout" />
+            <p className="sprout-points">+5</p>
             <img className="tree-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120221548686.png" alt="tree" />
+            <p className="tree-points">+40</p>
             <img className="well-img" src="https://nsa40.casimages.com/img/2021/05/11/210511101411434825.png" alt="well" />
+            <p className="well-points">+20</p>
             <img className="wheat-img" src="https://nsa40.casimages.com/img/2021/05/12/210512120139894866.png" alt="wheat" />
+            <p className="wheat-points">+10</p>
           </div>
         </div>
     )
